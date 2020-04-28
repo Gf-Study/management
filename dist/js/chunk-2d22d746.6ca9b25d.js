a455c64fe80663c175c5c6f46421c7c80ed04e1f
Gf-Study <42635043+Gf-Study@users.noreply.github.com>
2020-04-28 17:16:43 +0800

    Add files via upload

---
+++ dist/js/chunk-2d22d746.6ca9b25d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(e,t,n){"use strict";n.r(t);var c=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about container"},[n("h1",[e._v("一切都是最好的安排!")])])}],r={name:"About"},s=r,u=n("2877"),i=Object(u["a"])(s,c,a,!1,null,"32cfced8",null);t["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d22d746.6ca9b25d.js.map
